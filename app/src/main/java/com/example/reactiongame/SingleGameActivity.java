8bd6add8841aa31680b2a80bd63c394502eb28ce
carinas2 <carinas2@illinois.edu>
2020-05-02 16:03:27 -0500

    redButton

--- app/src/main/java/com/example/reactiongame/SingleGameActivity.java
+++ app/src/main/java/com/example/reactiongame/SingleGameActivity.java
@@ -1,6 +1,8 @@
 package com.example.reactiongame;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,18 @@ public class SingleGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single);
+        findViewById(R.id.redButton).setOnClickListener(unused -> dontPress());
+
     }
+
+
+    private void dontPress() {
+        findViewById(R.id.redButton).setVisibility(View.GONE);
+        findViewById(R.id.tooEarly).setVisibility(View.VISIBLE);
+        //findViewById(R.id.tooEarly).setOnClickListener(unused -> onCreate())
+
+    }
+
+
+
 }
