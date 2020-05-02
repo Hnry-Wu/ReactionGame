54528ae5292f0e9f3475e8a1b8e959f641f16885
carinas2 <carinas2@illinois.edu>
2020-05-02 17:06:50 -0500

    greenButton

--- app/src/main/java/com/example/reactiongame/MultiGameActivity.java
+++ app/src/main/java/com/example/reactiongame/MultiGameActivity.java
@@ -1,13 +1,51 @@
 package com.example.reactiongame;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
+
+import timerx.Timer;
+import timerx.TimerBuilder;
 
 public class MultiGameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi);
+        findViewById(R.id.redButton1).setOnClickListener(unused -> dontPress());
+
+
+
+        Timer timer = new TimerBuilder()
+        // Set start time
+        .startTime(5000, TimeUnit.MILLISECONDS)
+        // Set start format of time
+        .startFormat("SS.LL")
+        // Executing action
+        .actionWhen(0, TimeUnit.MILLISECONDS, () -> {
+            findViewById(R.id.redButton1).setVisibility(View.GONE);
+            findViewById(R.id.tooEarly1).setVisibility(View.GONE);
+            findViewById(R.id.greenButton1).setVisibility(View.VISIBLE);
+            findViewById(R.id.redButton2).setVisibility(View.GONE);
+            findViewById(R.id.tooEarly2).setVisibility(View.GONE);
+            findViewById(R.id.greenButton2).setVisibility(View.VISIBLE);
+            // Start stopwatch
+            //stopwatch.start();
+        })
+        .build();
+
+        timer.start();
     }
+
+    private void dontPress() {
+        findViewById(R.id.redButton).setVisibility(View.GONE);
+        findViewById(R.id.tooEarly).setVisibility(View.VISIBLE);
+        //findViewById(R.id.tooEarly).setOnClickListener(unused -> onCreate())
+    }
+
+
+
 }
--- app/src/main/java/com/example/reactiongame/SingleGameActivity.java
+++ app/src/main/java/com/example/reactiongame/SingleGameActivity.java
@@ -2,37 +2,55 @@ package com.example.reactiongame;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+//import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import timerx.Stopwatch;
+import timerx.StopwatchBuilder;
 import timerx.Timer;
 import timerx.TimerBuilder;
 
 public class SingleGameActivity extends AppCompatActivity {
+    private Stopwatch stopwatch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single);
         findViewById(R.id.redButton).setOnClickListener(unused -> dontPress());
+
+        stopwatch = new StopwatchBuilder()
+        // Set start format of time
+        .startFormat("SS.LL")
+        // When time is equal to one hour, change format to "HH:MM:SS"
+        .build();
+
+
         Random random = new Random();
-        int number = random.nextInt(3000) + 2000;
+        //int number = random.nextInt(3000) + 2000;
 
         Timer timer = new TimerBuilder()
         // Set start time
-        .startTime(number, TimeUnit.MILLISECONDS)
+        .startTime(5000, TimeUnit.MILLISECONDS)
         // Set start format of time
+        .startFormat("SS.LL")
+        // Executing action
         .actionWhen(0, TimeUnit.MILLISECONDS, () -> {
             findViewById(R.id.redButton).setVisibility(View.GONE);
+            findViewById(R.id.tooEarly).setVisibility(View.GONE);
             findViewById(R.id.greenButton).setVisibility(View.VISIBLE);
+            // Start stopwatch
+            stopwatch.start();
+
         })
         .build();
 
         timer.start();
+
 
     }
 
@@ -41,6 +59,12 @@ public class SingleGameActivity extends AppCompatActivity {
         findViewById(R.id.redButton).setVisibility(View.GONE);
         findViewById(R.id.tooEarly).setVisibility(View.VISIBLE);
         //findViewById(R.id.tooEarly).setOnClickListener(unused -> onCreate())
+    }
+
+    private void greenButtonPress() {
+        stopwatch.stop();
+        long timeTo = stopwatch.getTimeIn(TimeUnit.MILLISECONDS);
+
     }
 
 
