dc0fd5849b344585ecd75fd73a639bbfce07373f
carinas2 <carinas2@illinois.edu>
2020-05-02 16:27:26 -0500

    greenButton

--- app/src/main/java/com/example/reactiongame/SingleGameActivity.java
+++ app/src/main/java/com/example/reactiongame/SingleGameActivity.java
@@ -3,8 +3,15 @@ package com.example.reactiongame;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import timerx.Timer;
+import timerx.TimerBuilder;
 
 public class SingleGameActivity extends AppCompatActivity {
     @Override
@@ -12,6 +19,20 @@ public class SingleGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single);
         findViewById(R.id.redButton).setOnClickListener(unused -> dontPress());
+        Random random = new Random();
+        int number = random.nextInt(3000) + 2000;
+
+        Timer timer = new TimerBuilder()
+        // Set start time
+        .startTime(number, TimeUnit.MILLISECONDS)
+        // Set start format of time
+        .actionWhen(0, TimeUnit.MILLISECONDS, () -> {
+            findViewById(R.id.redButton).setVisibility(View.GONE);
+            findViewById(R.id.greenButton).setVisibility(View.VISIBLE);
+        })
+        .build();
+
+        timer.start();
 
     }
 
@@ -20,7 +41,6 @@ public class SingleGameActivity extends AppCompatActivity {
         findViewById(R.id.redButton).setVisibility(View.GONE);
         findViewById(R.id.tooEarly).setVisibility(View.VISIBLE);
         //findViewById(R.id.tooEarly).setOnClickListener(unused -> onCreate())
-
     }
 
 
