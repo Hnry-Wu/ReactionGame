d790c4f458a02ebec46a7ccda335f57f9fee6a2f
carinas2 <carinas2@illinois.edu>
2020-05-02 18:32:28 -0500

    multi mode

--- app/src/main/java/com/example/reactiongame/MultiGameActivity.java
+++ app/src/main/java/com/example/reactiongame/MultiGameActivity.java
@@ -2,15 +2,29 @@ package com.example.reactiongame;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import timerx.Stopwatch;
+import timerx.StopwatchBuilder;
 import timerx.Timer;
 import timerx.TimerBuilder;
 
 public class MultiGameActivity extends AppCompatActivity {
+    /**
+     * stores a stopwatch to keep track of reaction time
+     */
+    private Stopwatch stopwatch;
+
+    /**
+     * stores a timer to be used in the random waiting time
+     */
+    private Timer timer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +36,47 @@ public class MultiGameActivity extends AppCompatActivity {
         findViewById(R.id.redButton2).setVisibility(View.VISIBLE);
         findViewById(R.id.tooEarly2).setVisibility(View.GONE);
         findViewById(R.id.greenButton2).setVisibility(View.GONE);
+        findViewById(R.id.winner1).setVisibility(View.GONE);
+        findViewById(R.id.winner2).setVisibility(View.GONE);
 
         findViewById(R.id.redButton1).setOnClickListener(unused -> dontPress1());
         findViewById(R.id.redButton2).setOnClickListener(unused -> dontPress2());
 
-        Timer timer = new TimerBuilder()
+        // Declare new stopwatch object
+        stopwatch = new StopwatchBuilder()
+        // Set start format of time
+        .startFormat("SS.LL")
+        .build();
+        buildTimer();
+    }
+
+    /**
+     * runs if screen is tapped too early. prompts user to restart the game
+     */
+    private void dontPress1() {
+        findViewById(R.id.redButton1).setVisibility(View.GONE);
+        findViewById(R.id.tooEarly1).setVisibility(View.VISIBLE);
+        timer.stop();
+        findViewById(R.id.tooEarly1).setOnClickListener(unused -> startAgain());
+    }
+
+    private void dontPress2() {
+        findViewById(R.id.redButton2).setVisibility(View.GONE);
+        findViewById(R.id.tooEarly2).setVisibility(View.VISIBLE);
+        timer.stop();
+        findViewById(R.id.tooEarly2).setOnClickListener(unused -> startAgain());
+    }
+
+    /**
+     * builds a timer to randomly set amount of time players have to wait before they can react
+     */
+    private void buildTimer() {
+        Random random = new Random();
+        int number = random.nextInt(3000) + 2000;
+
+        timer = new TimerBuilder()
                 // Set start time
-                .startTime(5000, TimeUnit.MILLISECONDS)
+                .startTime(number, TimeUnit.MILLISECONDS)
                 // Set start format of time
                 .startFormat("SS.LL")
                 // Executing action
@@ -39,26 +87,62 @@ public class MultiGameActivity extends AppCompatActivity {
                     findViewById(R.id.redButton2).setVisibility(View.GONE);
                     findViewById(R.id.tooEarly2).setVisibility(View.GONE);
                     findViewById(R.id.greenButton2).setVisibility(View.VISIBLE);
+
+                    findViewById(R.id.greenButton1).setOnClickListener(unused -> greenButtonPress1());
+                    findViewById(R.id.greenButton2).setOnClickListener(unused -> greenButtonPress2());
                     // Start stopwatch
                     //stopwatch.start();
+
                 })
                 .build();
-
         timer.start();
     }
 
-    private void dontPress1() {
-        findViewById(R.id.redButton1).setVisibility(View.GONE);
-        findViewById(R.id.tooEarly1).setVisibility(View.VISIBLE);
+    /**
+     * resets the game if player wants to play again
+     */
+    private void startAgain() {
+        findViewById(R.id.redButton1).setVisibility(View.VISIBLE);
+        findViewById(R.id.tooEarly1).setVisibility(View.GONE);
         findViewById(R.id.greenButton1).setVisibility(View.GONE);
-    }
-
-    private void dontPress2() {
-        findViewById(R.id.redButton2).setVisibility(View.GONE);
-        findViewById(R.id.tooEarly2).setVisibility(View.VISIBLE);
+        findViewById(R.id.redButton2).setVisibility(View.VISIBLE);
+        findViewById(R.id.tooEarly2).setVisibility(View.GONE);
         findViewById(R.id.greenButton2).setVisibility(View.GONE);
+        findViewById(R.id.winner1).setVisibility(View.GONE);
+        findViewById(R.id.winner2).setVisibility(View.GONE);
+
+        findViewById(R.id.redButton1).setOnClickListener(unused -> dontPress1());
+        findViewById(R.id.redButton2).setOnClickListener(unused -> dontPress2());
+        buildTimer();
     }
 
+    /**
+     * once user reacts correctly, displays win screen with time and button to play again
+     */
+    private void greenButtonPress1() {
 
+        findViewById(R.id.redButton1).setVisibility(View.GONE);
+        findViewById(R.id.tooEarly1).setVisibility(View.GONE);
+        findViewById(R.id.greenButton1).setVisibility(View.GONE);
+        findViewById(R.id.redButton2).setVisibility(View.GONE);
+        findViewById(R.id.tooEarly2).setVisibility(View.GONE);
+        findViewById(R.id.greenButton2).setVisibility(View.GONE);
+
+        findViewById(R.id.winner1).setVisibility(View.VISIBLE);
+        findViewById(R.id.winner1).setOnClickListener(unused -> startAgain());
+    }
+
+    private void greenButtonPress2() {
+
+        findViewById(R.id.redButton1).setVisibility(View.GONE);
+        findViewById(R.id.tooEarly1).setVisibility(View.GONE);
+        findViewById(R.id.greenButton1).setVisibility(View.GONE);
+        findViewById(R.id.redButton2).setVisibility(View.GONE);
+        findViewById(R.id.tooEarly2).setVisibility(View.GONE);
+        findViewById(R.id.greenButton2).setVisibility(View.GONE);
+
+        findViewById(R.id.winner2).setVisibility(View.VISIBLE);
+        findViewById(R.id.winner2).setOnClickListener(unused -> startAgain());
+    }
 
 }
